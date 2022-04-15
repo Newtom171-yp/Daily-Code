b47c124d04f1a3fe9be1313979f57834ef579648
Yash Patil <yworriedart@gmail.com>
2022-04-15 13:06:27 +0530

    Intersection of two arrays [CodeStudio] modified.

--- Intersection_of_two_arrays.cpp
+++ Intersection_of_two_arrays.cpp
@@ -2,11 +2,10 @@ vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, i
 {
 	// Write your code here.
 	vector<int> arr;
-	int min = (arr1.size() < arr2.size()) ? arr1.size() : arr2.size();
 	int first=0,sec=0;
 	while(1)
 	{
-		if(first == arr1.size() || sec == arr2.size()) break;
+		if(first == n || sec == m) break;
 		if(arr1[first] < arr2[sec]) 
 		{
 			first++;
