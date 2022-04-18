b6be45d5af1f9b611eca421628f5478a0ca49fb4
Yash Patil <yworriedart@gmail.com>
2022-04-18 09:40:29 +0530

    Pivot C++ Program added.

---
+++ pivot.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int pivot(int a[], int size){
+    int l = 0;
+    int r = size - 1;
+    int mid = l + (r - l)/2;
+    while(l<r){
+        if(a[mid]>=0){
+            l = mid + 1;
+        }
+        else{
+            r = mid;
+        }
+        mid = l + (r - l)/2;
+    }
+    return r;
+}
+
+int main(){
+    int arr[] = {3, 8, 10, 17, 1, -1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    cout << "The pivot element is at index: "<< pivot(arr, size)<<endl;
+}
