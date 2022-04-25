17de9b5e9b80ca377fa482e9ec141ad211dfd2c3
Yash Patil <yworriedart@gmail.com>
2022-04-25 19:52:04 +0530

    Check if the string is a Palindrome [Code Studio] added.

---
+++ Check_if_the_string_is_a_Palindrome.cpp
@@ -0,0 +1,22 @@
+bool isValid(char ch){
+	return ((ch>='a'&&ch<='z') || (ch>='0' && ch<='9'));
+}
+
+bool checkPalindrome(string s)
+{
+	for(int i = 0; i < s.length()-1 ; i++){
+		if(s[i] >= 'A' && s[i] <= 'Z') s[i] += 'a' - 'A';
+	}
+	int l = 0;
+	int r = s.length()-1;
+	while(l<=r){
+		if(!isValid(s[l])) l++;
+		else if(!isValid(s[r])) r--;
+		else if(s[l] != s[r]) return false;
+		else{
+			l++;r--;
+		}
+	}
+	return true;
+	
+}
