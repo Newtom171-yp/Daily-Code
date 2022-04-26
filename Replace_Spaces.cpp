ec9d2c85135ab630b64d026f1a637643e557adea
Yash Patil <yworriedart@gmail.com>
2022-04-26 12:35:17 +0530

    Replace Spaces [Code Studio] done in O(1) extra space.

--- Replace_Spaces.cpp
+++ Replace_Spaces.cpp
@@ -1,13 +1,9 @@
 string replaceSpaces(string &str){
-	string temp = "";
 	for(int i = 0; i < str.length(); i++){
 		if(str[i] == ' '){
-			temp.push_back('@');
-			temp.push_back('4');
-			temp.push_back('0');
+			str.erase(i,1);
+			str.insert(i,"@40");
 		}
-		else temp.push_back(str[i]);
 	}
-	
-	return temp;
+	return str;
 }
