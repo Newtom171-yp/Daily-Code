238a4e5a18004fd825f7ead7f56fe41764a0d2fb
Yash Patil <yworriedart@gmail.com>
2022-04-26 12:40:09 +0530

    Replace Space [Code Studio] done in O(1) space complexity and with str.replace() function

--- Replace_Spaces.cpp
+++ Replace_Spaces.cpp
@@ -1,8 +1,7 @@
 string replaceSpaces(string &str){
 	for(int i = 0; i < str.length(); i++){
 		if(str[i] == ' '){
-			str.erase(i,1);
-			str.insert(i,"@40");
+			str.replace(i,1,"@40");
 		}
 	}
 	return str;
