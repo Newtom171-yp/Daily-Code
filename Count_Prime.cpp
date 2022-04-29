c79f9cd9338790884453f746894980d92e728b31
Yash Patil <yworriedart@gmail.com>
2022-04-29 09:14:47 +0530

    Count Prime [Leet Code] corrected.

--- Count_Prime.cpp
+++ Count_Prime.cpp
@@ -3,17 +3,17 @@ public:
     int countPrimes(int n) {
         int count = 0;
         
-        bool isPrime[5000000]={0};
+        bool isNotPrime[5000000]={0};
         
         for(int i = 2; i*i < n; i++){
-            if(isPrime[i]) continue;
+            if(isNotPrime[i]) continue;
             for(int j = i*i; j < n; j+=i){
-                isPrime[j] = true;
+                isNotPrime[j] = true;
             }
         }
         
         for(int i = 2; i < n; i++){
-            if(!isPrime[i]) count++;
+            if(!isNotPrime[i]) count++;
         }
         return count;
     }
