4360d69cfde2443aeb0a558f3af09ff1612d549b
Yash Patil <yworriedart@gmail.com>
2022-05-14 23:01:43 +0530

    Quick Sort added.

---
+++ Quick_Sort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+int partition(int *arr, int l, int r)
+{
+
+    int pivot = *(arr+l);
+    int count = 0;
+
+    for (int i = l+1; i <= r; i++)
+        if (*(arr + i) < pivot)
+            count++;
+
+    swap(*(arr + l), *(arr + l + count));
+
+    int pivotIndex = count + l;
+    int i = l, j = r;
+    while (i != j)
+    {
+        while(arr[i] < arr[pivotIndex]) i++;
+        while(arr[j] > arr[pivotIndex]) j--;
+        if(i != j) swap(arr[i],arr[j]);
+
+    }
+    return pivotIndex;
+}
+
+void sort(int *arr, int l, int r)
+{
+
+    if (l >= r)
+        return;
+
+    int p = partition(arr, l, r);
+
+    sort(arr, l, p - 1);
+    sort(arr, p + 1, r);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int *arr = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
