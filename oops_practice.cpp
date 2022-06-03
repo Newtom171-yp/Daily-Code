2577e713bc4b65f29fae27b82b0f26e252f613f8
Yash Patil <yworriedart@gmail.com>
2022-06-03 21:16:03 +0530

    oops practice updated.

---
+++ oops_practice.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class HERO
+{
+    // properties
+    int health = 100;
+    char level = 3;
+
+public:
+    int gethealth()
+    {
+        return health;
+    }
+
+    int getlevel()
+    {
+        return level;
+    }
+
+    void sethealth(int h)
+    {
+        health = h;
+    }
+
+    void setlevel(int l)
+    {
+        level = l;
+    }
+};
+
+int main()
+{
+    HERO Shaktiman;
+    cout << "Health before setting: " << Shaktiman.gethealth() << endl;
+    Shaktiman.sethealth(10);
+    cout << "Health after setting: " << Shaktiman.gethealth() << endl;
+
+    HERO *Nagraj = new HERO;
+    cout << "Health before setting: " << Nagraj->gethealth() << endl;
+    Nagraj->sethealth(101);
+    cout << "Health after setting: " << (*Nagraj).gethealth() << endl;
+    return 0;
+}
