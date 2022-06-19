f3a6b4a8eb0e51ba34c5172bc6a6053df3a810ea
Yash Patil <yworriedart@gmail.com>
2022-06-19 17:05:15 +0530

    Linkedlist practice updated.

--- Linked List/Doubly_Linkedlist.cpp
+++ Linked List/Doubly_Linkedlist.cpp
@@ -27,53 +27,104 @@ void print(Node *&head)
     cout << endl;
 }
 
-int getListLength(Node* &head)
+void getListLength(Node *&head)
 {
     int count = 0;
 
-    Node* temp = head;
+    Node *temp = head;
 
-    while(temp)
+    while (temp)
     {
         temp = temp->next;
         count++;
     }
 
-    return count;
+    cout <<"The length of the list is: " << count << endl;
 }
 
-void InsertAtHead(Node* &head, int data)
+void InsertAtHead(Node *&head, Node* & tail, int data)
 {
-    Node* temp = new Node(data);
+    if(!head)
+    {
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    Node *temp = new Node(data);
     temp->next = head;
     head->prev = temp;
     head = temp;
 }
 
-void InsertAtTail(Node* &tail, int data)
+void InsertAtTail(Node* &head, Node *&tail, int data)
 {
-    Node* temp = new Node(data);
+    if(!tail)
+    {
+        Node* newNode = new Node (data);
+        tail = newNode;
+        head = newNode;
+        return;
+    }
+    Node *temp = new Node(data);
     tail->next = temp;
     temp->prev = tail;
     tail = temp;
 }
 
+void InsertAtPosition(Node* &head, Node* &tail, int data, int pos)
+{
+    if(pos < 0)
+    {
+        cout<<"Invalid Index! "<<endl;
+        return;
+    }
+    if (!pos)
+    {
+        InsertAtHead(head, tail, data);
+        return;
+    }
+
+    Node *temp = head;
+
+    while (pos > 1)
+    {
+        temp = temp->next;
+        if (!temp)
+        {
+            cout << "Position out of the Linkedlist length, can't add the element! " << endl;
+            return;
+        }
+        pos--;
+    }
+
+    if (!temp->next)
+    {
+        InsertAtTail(head, tail, data);
+        return;
+    }
+
+    Node *newNode = new Node(data);
+    newNode->next = temp->next;
+    temp->next->prev = newNode;
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
 int main()
 {
-    Node* node = new Node(100);
-    Node* head = node;
-    Node* tail = node;
+    Node *head = NULL;
+    Node *tail = NULL;
 
-    cout << getListLength(head) << endl;
-    print(head);
-
-    InsertAtHead(head, 55);
-    InsertAtHead(head, 88);
-    InsertAtHead(head, 59);
-    InsertAtHead(head, 9);
-    InsertAtTail(tail, 69);
-    InsertAtTail(tail, 987);
-    InsertAtTail(tail, 76);
+    InsertAtHead(head, tail, 55);
+    InsertAtHead(head, tail, 88);
+    InsertAtHead(head, tail, 59);
+    InsertAtHead(head, tail, 9);
+    InsertAtTail(head, tail, 69);
+    InsertAtTail(head, tail, 987);
+    InsertAtTail(head, tail, 76);
+    InsertAtPosition(head, tail, 101, 5);
+    getListLength(head);
     print(head);
 
     return 0;
