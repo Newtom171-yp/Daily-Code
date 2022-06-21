ed32e9841d82a7966541d6b058e52cc7fca5c8e8
Yash Patil <yworriedart@gmail.com>
2022-06-21 20:47:22 +0530

    Linkedlist practice updated.

--- Linked List/Doubly_Linkedlist.cpp
+++ Linked List/Doubly_Linkedlist.cpp
@@ -12,7 +12,77 @@ public:
     {
         this->data = data;
     }
+
+    ~Node()
+    {
+        int val = this->data;
+        if(next)
+        {
+            delete next;
+            next = NULL;
+        }
+        if(prev)
+        {
+            delete prev;
+            prev = NULL;
+        }
+        cout<<"The value which got deleted was : "<<val<<endl;
+    }
 };
+
+int getListLength(Node *&head)
+{
+    int count = 0;
+
+    Node *temp = head;
+
+    while (temp)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    return count;
+}
+
+void deleteNode(Node* &head, Node* &tail, int position)
+{
+    if(position<0 || position > getListLength(head)-1)
+    {
+        cout<<"Invalid Position!"<<endl;
+    }
+    if(!position)
+    {
+        Node* temp = head;
+        head = head->next;
+        temp->next->prev = NULL;
+        temp->next = NULL;
+        delete temp;
+        return;
+    }
+
+    if(position+1 == getListLength(head))
+    {
+        Node*temp = tail;
+        tail = tail->prev;
+        temp->prev->next = NULL;
+        temp->prev = NULL;
+        delete temp;
+        return;
+    }
+
+    Node* temp = head;
+    while(position--)
+    {
+        temp = temp->next;
+    }
+
+    temp->prev->next = temp->next;
+    temp->next->prev = temp->prev;
+    temp->next = NULL;
+    temp->prev = NULL;
+    delete temp;
+}
 
 void print(Node *&head)
 {
@@ -27,20 +97,6 @@ void print(Node *&head)
     cout << endl;
 }
 
-void getListLength(Node *&head)
-{
-    int count = 0;
-
-    Node *temp = head;
-
-    while (temp)
-    {
-        temp = temp->next;
-        count++;
-    }
-
-    cout <<"The length of the list is: " << count << endl;
-}
 
 void InsertAtHead(Node *&head, Node* & tail, int data)
 {
@@ -124,7 +180,8 @@ int main()
     InsertAtTail(head, tail, 987);
     InsertAtTail(head, tail, 76);
     InsertAtPosition(head, tail, 101, 5);
-    getListLength(head);
+    print(head);
+    deleteNode(head,tail,6);
     print(head);
 
     return 0;
