bcca673b237231c53fa5277e5f8c692dcc576846
Yash Patil <yworriedart@gmail.com>
2022-07-05 16:58:57 +0530

    stack practice 3 added.

--- Stack/stack_practice_3.cpp
+++ Stack/stack_practice_3.cpp
@@ -5,35 +5,17 @@ class stack
 {
     int data;
     stack* next;
-    stack* top = NULL;
-
+    
     public:
 
     stack(){};
-
     stack(int data)
     {
         this->data = data;
         next = NULL;
     }
 
-    ~stack()
-    {
-
-        if(next)
-        {
-            delete next;
-            next = NULL;
-        }
-
-        if(top)
-        {
-            delete top;
-            top = NULL;
-        }
-    }
-
-    void push(int data)
+    void push(int data, stack* &top)
     {
         stack* newNode = new stack(data);
 
@@ -45,10 +27,9 @@ class stack
 
         newNode->next = top;
         top = newNode;
-        return;
     }
 
-    void pop()
+    void pop(stack* &top)
     {
         if(!top)
         {
@@ -57,26 +38,43 @@ class stack
         }
 
         stack* temp = top;
-
         top = top->next;
-
-        cout << "The poped element is : "<< temp->data<<endl;
-
+        cout<<"The poped element is : "<< temp->data << endl;
+        temp->next = NULL;
         delete temp;
     }
 
-    int peek()
+    int peek(stack* &top)
     {
+        if(!top)
+        {
+            cout<<"Stack Underflow!"<<endl;
+            return -1;
+        }
         return top->data;
+    }
+
+    bool isEmpty(stack* &top)
+    {
+        return !top;
     }
 };
 
 int main()
 {
+    stack* top = NULL;
     stack s;
-    s.push(8);
-    s.pop();
-    printf("The top element is : ", s.peek());
+    s.push(10 , top);
+    s.push(20, top);
+    s.push(30, top);
+    cout <<s.peek(top) << endl;
 
+    s.pop(top);
+    s.pop(top);
+    s.pop(top);
+
+    cout << s.peek(top) << endl;
+
+    cout << s.isEmpty(top) << endl;
     return 0;
 }
