355e459480f8244c11f68f2def1a9fd54b5494b0
Yash Patil <yworriedart@gmail.com>
2022-07-25 14:54:06 +0530

    queue reversal [GFG] approach 1 added.

---
+++ Queue/queue_reversal_approach1.cpp
@@ -0,0 +1,16 @@
+queue<int> rev(queue<int> q)
+{
+    // add code here.
+    stack<int> s;
+    while(!q.empty())
+    {
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty())
+    {
+        q.push(s.top());
+        s.pop();
+    }
+    return q;
+}
