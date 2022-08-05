31cf883c81e0007743d9b55368aca59cffc38840
Yash Patil <yworriedart@gmail.com>
2022-08-05 19:37:53 +0530

    diagonal traversal of binary tree [GFG] added.

---
+++ Tree/diagonal_traversal_of_binary_tree.cpp
@@ -0,0 +1,21 @@
+vector<int> diagonal(Node *root)
+{
+   // your code here
+   vector<int> ans;
+   queue<Node*> q;
+   
+   q.push(root);
+   
+   while(!q.empty())
+   {
+       Node* temp = q.front();
+       q.pop();
+       while(temp)
+       {
+           if(temp->left) q.push(temp->left);
+           ans.push_back(temp->data);
+           temp = temp->right;
+       }
+   }
+   return ans;
+}
