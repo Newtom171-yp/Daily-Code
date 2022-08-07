2b7595578cc56f34f5aacbec5464bd18ebf68ebd
Yash Patil <yworriedart@gmail.com>
2022-08-07 09:25:22 +0530

    sum of the longes bloodline of a tree [GFG] added.

---
+++ Tree/sum_of_the_longest_bloodline_of_a_tree.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+public:
+
+    void solve(Node* &root, int sum, int &maxSum, int len, int &maxLen)
+    {
+        if(!root)
+        {
+            if(len > maxLen) 
+            {
+                maxLen = len;
+                maxSum = sum;
+            }
+            else if(len == maxLen)
+            {
+                maxSum = max(sum, maxSum);
+            }
+            return;
+        }
+        
+        sum += root->data;
+        
+        solve(root->left, sum, maxSum, len+1, maxLen);
+        solve(root->right, sum, maxSum, len+1, maxLen);
+        
+    }
+
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        //code here
+        int len = 0;
+        int maxLen = 0;
+        
+        
+        int sum = 0;
+        int maxSum = INT_MIN;
+        
+        solve(root, sum, maxSum, len, maxLen);
+        
+        return maxSum;
+    }
+};
